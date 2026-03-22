1aadfe9cd224055357fc94524c0f23f6f9b2cd52
Shiv <shiv.shaw@gmail.com>
2026-03-22 23:03:18 +0530

    healthcheck api added

---
+++ src/app.js
@@ -0,0 +1,35 @@
+import express from "express"
+import cors from "cors"
+
+
+const app = express();
+
+
+// Basic Config middlleware
+app.use(express.json({ limit : "16kb" }))
+app.use(express.urlencoded({ 
+    extended: true,
+    limit: "16kb"
+}))
+app.use(express.static("public"))
+// Cors
+app.use(cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET","POST","PUT","PATCH","DELETE","OPTIONS"],
+    allowedHeaders: ["Content-Type","Authorization"]
+}))
+
+
+// Route Setup
+import healthCheckRoute from "./routes/healthcheck.routes.js";
+
+app.use("/api/v1/healthcheck",healthCheckRoute);
+
+
+
+app.get("/",(req, res) => {
+    res.send('Hey !!');
+})
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log("Server running ...")
+import dotenv from "dotenv"
+import app from "./app.js"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "./.env",
+})
+
+
+const port = process.env.PORT || 3000;
+
+
+connectDB()
+.then(() => {
+    app.listen(port, ()=>{
+    console.log(`Server Listening on Port : ${port}.`);
+})
+})
+.catch((err) => {
+    console.error("MongoDB Connection Error ", err);
+    process.exit(1);
+})
